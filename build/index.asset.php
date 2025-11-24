d95d8b95087055ad65b93251ecd55426841daacb
panchenkogit <odosaol@yandex.ru>
2025-11-24 16:16:20 +0500

    компоненты для настроек на sidebar

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-date'), 'version' => '0b2fd1719b88cd52f4a9');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-date'), 'version' => '24f53788dcaa3328dbe8');
--- src/edit.js
+++ src/edit.js
@@ -1,13 +1,15 @@
-import { useBlockProps } from '@wordpress/block-editor';
+import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
 import { useSelect } from '@wordpress/data';
+import { PanelBody, ToggleControl, QueryControls } from '@wordpress/components';
 import { format, dateI18n, getSettings } from '@wordpress/date';
 
 import './editor.scss';
 
-export default function Edit({ attributes }) {
+export default function Edit({ attributes, setAttributes }) {
 	const { postsPerPage, showImage } = attributes;
-	const posts = useSelect((select) => {
-		return select('core').getEntityRecords(
+
+	const posts = useSelect((select) =>
+		select('core').getEntityRecords(
 			'postType',
 			'post',
 			{
@@ -15,38 +17,61 @@ export default function Edit({ attributes }) {
 				_embed: true,
 			},
 			[postsPerPage]
-		);
-	});
+		)
+	);
+
 	const blockProps = useBlockProps();
+
+	const onChangeToggleImage = (value) => {
+		setAttributes({ showImage: value });
+	};
+	const onChangePostsPerPage = (value) => {
+		setAttributes({ postsPerPage: value });
+	};
+
 	return (
-		<div {...blockProps}>
-			{posts &&
-				posts.map((post) => {
+		<>
+			<InspectorControls>
+				<PanelBody title='Settings'>
+					<ToggleControl
+						label='Display Images'
+						checked={showImage}
+						onChange={onChangeToggleImage}
+					/>
+					<QueryControls
+						numberOfItems={postsPerPage}
+						onNumberOfItemsChange={onChangePostsPerPage}
+						maxItems={10}
+						minItems={1}
+					/>
+				</PanelBody>
+			</InspectorControls>
+
+			<div {...blockProps}>
+				{posts?.map((post) => {
 					const featuredImage =
-						post._embedded &&
-						post._embedded['wp:featuredmedia'] &&
-						post._embedded['wp:featuredmedia'].length > 0 &&
-						post._embedded['wp:featuredmedia'][0];
+						post._embedded?.['wp:featuredmedia']?.[0] ?? null;
+					const imageUrl =
+						featuredImage?.media_details?.sizes?.large?.source_url ?? '';
+					const imageAlt = featuredImage?.alt_text ?? '';
 
 					return (
 						<div key={post.id}>
-							{showImage && featuredImage && (
-								<img
-									src={featuredImage.media_details.sizes.large.source_url}
-									alt={featuredImage.alt_text}
-								/>
-							)}
+							{showImage && imageUrl && <img src={imageUrl} alt={imageAlt} />}
+
 							{post.date_gmt && (
 								<time dateTime={format('c', post.date_gmt)}>
 									{dateI18n(getSettings().formats.date, post.date_gmt)}
 								</time>
 							)}
+
 							<h2>
 								<a href={post.link}>{post.title.rendered}</a>
 							</h2>
 						</div>
 					);
 				})}
-		</div>
+			</div>
+		</>
 	);
 }
